a44251f628c762cef5dd2fae51f093826a3f09e8
Habin Park <HabinPark@community.int>
2022-03-21 15:54:25 -0400

    cake

---
+++ cake.py
@@ -0,0 +1,8 @@
+cakeOrBust = input("Do you want some cake? (yes/no)")
+
+if cakeOrBust == 'yes':
+    print('Here is some cake')
+elif cakeOrBust == 'no':
+    print('No cake for you')
+else:
+    print("I don't understand")
