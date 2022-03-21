177ee9bec7e6ccd97ebd7bcbcd06183827052793
Habin Park <HabinPark@community.int>
2022-03-21 16:02:58 -0400

    added spanish

---
+++ spanish.py
@@ -0,0 +1,10 @@
+word = input("Give me a word in English...(cat/dog/horse): ")
+
+if word == 'cat':
+    print('gato')
+elif word == 'dog':
+    print('perro')
+elif word == 'horse':
+    print('caballo')
+else:
+    print("No entiendo")
